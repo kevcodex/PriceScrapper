93f079ac1ec189c7c84776e8f4f151e11925f0b1
Kevin Chen <kevchen2@gmail.com>
2018-12-13 22:30:41 -0800

    break out routes to a controller

--- Server/Sources/App/Controllers/ProductController.swift
+++ Server/Sources/App/Controllers/ProductController.swift
@@ -1,3 +1,45 @@
 import Vapor
 
-
+struct ProductController: RouteCollection {
+    func boot(router: Router) throws {
+        let apiRouter = router.grouped("api", "products")
+        apiRouter.get(use: getAllHandler)
+        apiRouter.post(use: createProductHandler)
+        apiRouter.get(Product.parameter, use: getProductHandler)
+        apiRouter.delete(Product.parameter, use: deleteHandler)
+    }
+    
+    func getAllHandler(_ req: Request) throws -> Future<[Product]> {
+        return Product.query(on: req).all()
+    }
+    
+    func createProductHandler(_ req: Request) throws -> Future<Product> {
+        
+        return try req.content.decode(Product.self).flatMap(to: Product.self) { product in
+            
+            guard let id = product.id else {
+                throw Abort(HTTPResponseStatus.badRequest)
+            }
+            
+            return Product.find(id, on: req)
+                .flatMap(to: Product.self, { (foundProduct) -> EventLoopFuture<Product> in
+                    
+                    if let foundProduct = foundProduct {
+                        foundProduct.priceHistory.append(contentsOf: product.priceHistory)
+                        
+                        return foundProduct.create(orUpdate: true, on: req)
+                    } else {
+                        return product.create(orUpdate: true, on: req)
+                    }
+                })
+        }
+    }
+    
+    func getProductHandler(_ req: Request) throws -> Future<Product> {
+        return try req.parameters.next(Product.self)
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(Product.self).delete(on: req).transform(to: HTTPStatus.noContent)
+    }
+}
--- Server/Sources/App/routes.swift
+++ Server/Sources/App/routes.swift
@@ -7,37 +7,5 @@ public func routes(_ router: Router) throws {
         return "It works!"
     }
     
-    router.post("products") { req -> Future<Product> in
-        
-        return try req.content.decode(Product.self).flatMap(to: Product.self) { product in
-            
-            guard let id = product.id else {
-                throw Abort(HTTPResponseStatus.badRequest)
-            }
-            
-            return Product.find(id, on: req)
-                .flatMap(to: Product.self, { (foundProduct) -> EventLoopFuture<Product> in
-                    
-                    if let foundProduct = foundProduct {
-                        foundProduct.priceHistory.append(contentsOf: product.priceHistory)
-                        
-                        return foundProduct.create(orUpdate: true, on: req)
-                    } else {
-                        return product.create(orUpdate: true, on: req)
-                    }
-                })
-        }
-    }
-    
-    router.get("products") { req in
-        return Product.query(on: req).all()
-    }
-    
-    router.get("products", Product.parameter) { req in
-        return try req.parameters.next(Product.self)
-    }
-    
-    router.delete("products", Product.parameter) { req in
-        return try req.parameters.next(Product.self).delete(on: req).transform(to: HTTPStatus.noContent)
-    }
+    try router.register(collection: ProductController())
 }
