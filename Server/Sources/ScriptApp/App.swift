2028c99b042c47dcc193128f4a63854951e5eea5
Kevin Chen <kevchen2@gmail.com>
2019-01-16 12:27:24 -0800

    Setup basic structure for database updating

--- Server/Sources/ScriptApp/App.swift
+++ Server/Sources/ScriptApp/App.swift
@@ -24,17 +24,15 @@ public class App {
         
         let worker = MultiThreadedEventLoopGroup(numberOfThreads: 2)
         
-        let conn = try database.newConnection(on: worker)
+        let connection = database.newConnection(on: worker)
         
-        let test = conn.flatMap { connection in
-            return Product.query(on: connection).all()
-        }
-        
-        let blah = try test.wait()
-        
-        print(blah.first?.asin)
-        
-        try worker.syncShutdownGracefully()
+//        let test = conn.flatMap { connection in
+//            return Product.query(on: connection).all()
+//        }
+//
+//        let blah = try test.wait()
+//
+//        print(blah.first?.asin)
 
         let request1 = AmazonRequest(baseURL: URL(string: "https://www.amazon.com"), path: "/Rosewill-Computer-Tempered-Liquid-Cooling-Pre-Installed/dp/B01M6TV6PC")
         
@@ -45,7 +43,28 @@ public class App {
         let controller = ScrapperController(requests: [request1, request2, request3])
         
         let products = controller.retrieveProductsInfo()
-        print(products)
-        print("dawdw")
+        for product in products {
+            let asin = product.asin
+            
+            let database = try connection.wait()
+            
+            let foundFutureProduct = Product.query(on: database).filter(\.asin == asin).first()
+            
+            let foundProduct = try foundFutureProduct.wait()
+            
+            if let foundProduct = foundProduct {
+                foundProduct.priceHistory.append(contentsOf: product.priceHistory)
+                
+                foundProduct.create(orUpdate: true, on: database).always {
+                    // Do nothing for now
+                }
+            } else {
+                product.create(orUpdate: true, on: database).always {
+                    // Do nothing for now
+                }
+            }
+        }
+
+        try worker.syncShutdownGracefully()
     }
 }
